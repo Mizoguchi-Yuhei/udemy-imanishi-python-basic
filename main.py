aef25bc762e354ecf65bc820f933119a4a172e4e
Mizoguchi-Yuhei <mizo.cb.fl@gmail.com>
2021-08-24 14:24:07 +0900

    3-13

--- main.py
+++ main.py
@@ -1,27 +1,64 @@
 import streamlit as st
-import numpy as np
-import pandas as pd
-from PIL import Image
+# import numpy as np
+# import pandas as pd
+# from PIL import Image
+import time
 
 st.title('Streamlit 超入門')
 
 # st.write('DataFrame')
 # st.write('Display Image')
-st.write('Interactive Widgets')
+# st.write('Interactive Widgets')
+st.sidebar.write('Sidebar')
+st.write('プログレスバーの表示')
+'Start!!'
+
+latest_interation = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+  latest_interation.text(f'Interation {i + 1}')
+  bar.progress(i + 1)
+  time.sleep(0.01)
+
+'Done!!'
 
 
-# インタラクティブなウィジェット
-text = st.text_input('あなたの趣味を教えてください。')
-'あなたの趣味：', text
+left_column, right_column = st.columns(2)
+button = left_column.button('右カラムに文字を表示')
+if button:
+  right_column.write('ここは右カラム')
 
-option = st.selectbox(
+expandar1 = st.expander('お問い合わせ1')
+expandar1.write('お問い合わせ1回答')
+expandar2 = st.expander('お問い合わせ2')
+expandar2.write('お問い合わせ2回答')
+
+text = st.sidebar.text_input('あなたの趣味を教えてください。')
+
+option = st.sidebar.selectbox(
   'あなたが好きな数字を教えてください。',
   list(range(1, 100))
 )
-'あなたの好きな数字は「', option, '」です。'
 
-condition = st.slider('あなたの今の調子は？', 0, 100, 50)
+condition = st.sidebar.slider('あなたの今の調子は？', 0, 100, 50)
+
+'あなたの趣味：', text
+'あなたの好きな数字は「', option, '」です。'
 'コンディション：', condition
+
+# インタラクティブなウィジェット
+# text = st.text_input('あなたの趣味を教えてください。')
+# 'あなたの趣味：', text
+
+# option = st.selectbox(
+#   'あなたが好きな数字を教えてください。',
+#   list(range(1, 100))
+# )
+# 'あなたの好きな数字は「', option, '」です。'
+
+# condition = st.slider('あなたの今の調子は？', 0, 100, 50)
+# 'コンディション：', condition
 
 # 画像表示
 # if st.checkbox('Show Image'):
